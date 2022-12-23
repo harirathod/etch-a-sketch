12ca752362e871f6f4a161403188da2e580276ec
harirathod <h4ri.rathod@gmail.com>
2022-12-23 11:39:15 +0000

    Add reset button

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let container = document.querySelector('#grid-container');
 createGrid(32);
-
 function createGrid(n) 
 {
     let width = container.clientWidth / n;
@@ -14,5 +13,36 @@ function createGrid(n)
         }
         container.appendChild(rowDiv);
     }
+    getGridBoxes().forEach(div => div.addEventListener('mouseover', function(e) {
+        e.target.classList.add('coloured');
+    }));
+}
+
+function reset()
+{
+    let rows = document.querySelectorAll('#grid-container > div');
+    rows.forEach(row => row.remove());
+}
+
+let resetBtn = document.querySelector('#change-grid');
+resetBtn.addEventListener('click', function(e) {
+    let squaresPerSide = 32;
+    squaresPerSide = prompt('Enter a valid number for the squares per side of the new grid.', "32");
+    while(squaresPerSide > 100 || squaresPerSide < 5) {
+        squaresPerSide = prompt('Your number was not in the valid range (5 - 100). Please enter a valid number.', "32");
+    }
+    reset();
+    createGrid(squaresPerSide);
+})
+
+let multicolouredBtn = document.querySelector('#multicoloured');
+multicolouredBtn.addEventListener('click', function(e) {
+    getGridBoxes().forEach(div => div.classList.add('multicoloured'));
+})
+
+function getGridBoxes()
+{
+    let divs = document.querySelectorAll('#grid-container > div > div');
+    return divs;
 }
 
